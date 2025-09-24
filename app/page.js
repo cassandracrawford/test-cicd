330bf807dc8ad7428fbfdcf63ece47babcf866c3
cassandracrawford <crfeliciano1989@gmail.com>
2025-09-24 17:05:19 -0600

    Update page.js

--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ export default function Home() {
     <div className="flex justify-center items-center bg-amber-100 h-screen">
       <h1 className="text-4xl font-extrabold text-amber-900">TEST FOR CI/CD</h1>
       <p>this is a change</p>
+      <p>change</p>
     </div>
   );
 }

e93cb5db015b61c50f4bf5678c48d581f1da2a0e
cassandracrawford <crfeliciano1989@gmail.com>
2025-09-24 20:13:05 -0600

    change added

--- app/page.js
+++ app/page.js
@@ -3,9 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex justify-center items-center bg-amber-100 h-screen">
-      <h1 className="text-4xl font-extrabold text-amber-900">TEST FOR CI/CD</h1>
-      <p>this is a change</p>
-      <p>this is a change</p>
+      <p className="text-4xl font-extrabold text-amber-900">TEST FOR CI/CD</p>
+      <p className="">this is a change</p>
     </div>
   );
 }
